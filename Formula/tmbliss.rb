b667c09b10960d76b33074a9f17a63388c9743f6
Vyrtsev Mikhail <reeywhaar@gmail.com>
2023-05-13 23:54:38 +0300

    Configure tmbliss service

--- Formula/tmbliss.rb
+++ Formula/tmbliss.rb
@@ -5,10 +5,24 @@ class Tmbliss < Formula
 
 	version "0.0.1-beta.3"
 
+	revision 4
+
 	depends_on :macos => :high_sierra
 
 	url "https://github.com/Reeywhaar/tmbliss/releases/download/v0.0.1-beta.3/homebrew.zip"
 	sha256 "a769ed5230b8904b97113b2e27e58d1ae97a61d65cba7b0d13cbbcea9a29a0cb"
+
+	service do
+		run ["#{opt_bin}/tmbliss", "conf", "--path", "#{etc}/tmbliss.conf"]
+		run_type :interval
+		interval 36000 # 10 hours
+		log_path "#{var}/log/tmbliss.log"
+		error_log_path "#{var}/log/tmbliss.error.log"
+	end
+
+	test do
+		system "#{bin}/tmbliss", "help"
+	end
 
 	def install
 		on_arm do
@@ -17,9 +31,27 @@ class Tmbliss < Formula
 		on_intel do
 			bin.install "intel/tmbliss"
 		end
+
+		create_conf
+
+		etc.install "tmbliss.conf"
 	end
 
-	test do
-		system "#{bin}/tmbliss", "help"
+	private
+
+	def create_conf
+		File.open("tmbliss.conf", "w") do |f|     
+			content = <<~TEXT
+			{
+				"paths": [],
+				"dry_run": true,
+				"whitelist_glob": [],
+				"whitelist_path": [],
+				"skip_glob": [],
+				"skip_path": []
+			}
+			TEXT
+			f.write(content)   
+		end
 	end
 end
