b251f46d74e78d7c784ed5f33d81c2464f5e0a48
Vyrtsev Mikhail <reeywhaar@gmail.com>
2023-05-14 01:37:15 +0300

    Audit tmbliss formula

--- Formula/tmbliss.rb
+++ Formula/tmbliss.rb
@@ -1,16 +1,13 @@
 class Tmbliss < Formula
   desc "Exclude development files from Time Machine backups"
   homepage "https://github.com/reeywhaar/tmbliss"
-  license "MIT"
-
-  version "0.0.1-beta.3"
-
-  revision 4
-
-  depends_on :macos => :high_sierra
 
   url "https://github.com/Reeywhaar/tmbliss/releases/download/v0.0.1-beta.3/homebrew.zip"
+  version "0.0.1-beta.3"
   sha256 "a769ed5230b8904b97113b2e27e58d1ae97a61d65cba7b0d13cbbcea9a29a0cb"
+  license "MIT"
+  revision 4
+  depends_on macos: :high_sierra
 
   service do
     run ["#{opt_bin}/tmbliss", "conf", "--path", "#{etc}/tmbliss.conf"]
@@ -20,38 +17,34 @@ class Tmbliss < Formula
     error_log_path "#{var}/log/tmbliss.error.log"
   end
 
-  test do
-    system "#{bin}/tmbliss", "help"
-  end
-
   def install
-    on_arm do
-        bin.install "silicon/tmbliss"
-    end
-    on_intel do
-        bin.install "intel/tmbliss"
-    end
+    bin.install "silicon/tmbliss" if Hardware::CPU.arm?
+    bin.install "intel/tmbliss" if Hardware::CPU.intel?
 
     create_conf
 
     etc.install "tmbliss.conf"
   end
 
+  test do
+    system "#{bin}/tmbliss", "help"
+  end
+
   private
 
   def create_conf
-    File.open("tmbliss.conf", "w") do |f|     
+    File.open("tmbliss.conf", "w") do |f|
       content = <<~TEXT
-      {
-          "paths": [],
-          "dry_run": true,
-          "whitelist_glob": [],
-          "whitelist_path": [],
-          "skip_glob": [],
-          "skip_path": []
-      }
+        {
+            "paths": [],
+            "dry_run": true,
+            "allow_glob": [],
+            "allow_path": [],
+            "skip_glob": [],
+            "skip_path": []
+        }
       TEXT
-      f.write(content)   
+      f.write(content)
     end
   end
 end
