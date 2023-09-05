f0d47a25f6d1896fca90e6074520b131347c9340
Vyrtsev Mikhail <reeywhaar@gmail.com>
2023-09-06 01:11:12 +0300

    Add placeholders

--- Casks/middleme.rb
+++ Casks/middleme.rb
@@ -1,5 +1,7 @@
 cask "middleme" do
+  # version-placeholder
   version "1.5"
+  # sha256-placeholder
   sha256 "698a054bcc6fbb5b9773745b7f0f0a862e38d0533fd70f0db9f935c870a532c8"
 
   url "https://github.com/Reeywhaar/middleme/releases/download/#{version}/MiddleMe.zip"
