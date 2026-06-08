f027817c6c723a3bdcd93601e0475f2628c2d28f
runner <runner@example.com>
2026-06-08 04:34:12 +0000

    diff-tgz 0.1.2

--- Formula/diff-tgz.rb
+++ Formula/diff-tgz.rb
@@ -2,29 +2,29 @@ class DiffTgz < Formula
   desc "Compute and apply VCDIFF binary patches between tgz archives"
   homepage "https://github.com/Reeywhaar/diff-tgz"
   license "MIT"
-  version "0.1.1"
+  version "0.1.2"
 
   on_macos do
     on_intel do
-      url "https://github.com/Reeywhaar/diff-tgz/releases/download/0.1.1/diff-tgz-0.1.1-x86_64-apple-darwin.tar.gz"
-      sha256 "87c5766eccd9dd9ddceb5373a796fac972401cec5c684bffd12ee00ea2e8ed9c"
+      url "https://github.com/Reeywhaar/diff-tgz/releases/download/0.1.2/diff-tgz-0.1.2-x86_64-apple-darwin.tar.gz"
+      sha256 "f5387c568510b91ffecffd9a816b0f13dfb4e9896ad43f8d86860ca028057956"
     end
 
     on_arm do
-      url "https://github.com/Reeywhaar/diff-tgz/releases/download/0.1.1/diff-tgz-0.1.1-aarch64-apple-darwin.tar.gz"
-      sha256 "810adf0b6930163b2af3c56855aad220a570f33765b2443589261f3f47b07d7a"
+      url "https://github.com/Reeywhaar/diff-tgz/releases/download/0.1.2/diff-tgz-0.1.2-aarch64-apple-darwin.tar.gz"
+      sha256 "2b4f817c0cd7b6cf1b8b46da0d7595c4e1985e035309d0a1ef9bad6a98a5845e"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/Reeywhaar/diff-tgz/releases/download/0.1.1/diff-tgz-0.1.1-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "d86c2cbd1c0d8d7c3cb45312dbd8af5bde95c0155dde2bb70c4826109ba402d3"
+      url "https://github.com/Reeywhaar/diff-tgz/releases/download/0.1.2/diff-tgz-0.1.2-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "49facf64ad841dd8e7be31546f6ed940be6ee4e520b0b1a942981f368f2c6793"
     end
 
     on_arm do
-      url "https://github.com/Reeywhaar/diff-tgz/releases/download/0.1.1/diff-tgz-0.1.1-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "fb71301aa08122b99234cfd3e004588068d1673d1b4c63a0bfed93f84ee79fb2"
+      url "https://github.com/Reeywhaar/diff-tgz/releases/download/0.1.2/diff-tgz-0.1.2-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "8a2402ed544f4657d6f3e673635c301dcad24314d5ee4e26ad62f1b38b2eae00"
     end
   end
 
