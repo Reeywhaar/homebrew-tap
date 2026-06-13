cd9cc1468ade82d9a4d3ae2749f32a6f7287a768
runner <runner@example.com>
2026-06-13 10:35:35 +0000

    MiddleMe 1.11

--- Casks/middleme.rb
+++ Casks/middleme.rb
@@ -1,6 +1,6 @@
 cask "middleme" do
-  version "1.10"
-  sha256 "481171ce80606c63653856e6f1a7a7b799a03e59a8fa82e1356d016533529045"
+  version "1.11"
+  sha256 "42ea505d147d0559a195e1a92b0c78dbf52121f4675a28d557ea05180d2a6965"
 
   url "https://github.com/Reeywhaar/middleme/releases/download/#{version}/MiddleMe.zip"
   name "MiddleMe"
